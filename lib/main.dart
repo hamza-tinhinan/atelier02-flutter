ed25895c3cc5ea0781587665987bab8300e14a7e
adel bennacer <adelbennacer46gmail.com>
2025-11-14 12:46:07 -0500

    Etape 4 : Ajout d'un Stack avec Container, Icon et Text

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,54 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-         child: Column(
-         children: [
-          Text('Hello world'),
-          Text('bonjour tous le monde'),
-          Container(
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: Colors.blueAccent,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Text(
-              'boite stylisee avec contenaire',
-              style: TextStyle(color:Colors.white, fontSize: 18),
+          child: Column(
+            children: [
+              
+              Text('Hello world'),
+              Text('bonjour tous le monde'),
 
-            ),
-          )
-         ], 
-         ),
+              
+              Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  'boite stylisee avec contenaire',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+
+              
+              Text("bonsoir a tous"),
+
+              
+              Stack(
+                alignment: Alignment.center, 
+                children: [
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.blue[100],
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 100,
+                    color: Colors.amber,
+                  ),
+                  Text(
+                    "Superposé !",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
